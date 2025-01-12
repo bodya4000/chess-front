582c3a14b8d09ff7223d28613916641e82c2e2f5
Bohdan Shraier <bohdanshraier@Bohdans-MacBook-Air.local>
2025-01-12 23:48:01 +0200

    fixed king highlight moves

--- src/features/chess/services/CheckmateAnalyzer.ts
+++ src/features/chess/services/CheckmateAnalyzer.ts
@@ -4,7 +4,7 @@ import Color from '../models/enums/Color';
 import Figures from '../models/enums/Figures';
 import Figure from '../models/figures/Figure';
 import BoardFinder from '../utils/BoardFinder';
-import ChessHelper from '../utils/ChessHelper'
+import ChessHelper from '../utils/ChessHelper';
 import MoveAnalyzer from './MoveAnalyzer';
 import MoveEmulator from './MoveEmulator';
 
@@ -68,27 +68,76 @@ class CheckmateAnalyzer {
 	}
 
 	/**
-	 * Filters out moves that would leave the king in check.
+	 * Filters out moves that would leave the king in check, including special handling for castling.
 	 * @param board The current game board.
 	 * @param figure The figure for which to calculate safe moves.
 	 * @returns A list of valid moves for the figure that do not leave the king in check.
 	 */
 	getMovesWithoutCheck(board: Board, figure: Figure): Cell[] {
 		const figureMoves = figure.getFigureMoves();
-		return figureMoves.filter(cellToMove => {
+		let castlingMoves: Cell[] = [];
+		const filteredFigureMoves = figureMoves.filter(cellToMove => {
 			const figureCellBeforeMove = figure.getCell();
 			let capturedFigure: Figure | null;
+
 			if (figure.getFigureName() === Figures.Pawn && this.moveAnalyzer.isEnPassantMove(figure, cellToMove)) {
 				const capturedCell = ChessHelper.getEnPassantCapturedCell(figure, cellToMove);
 				capturedFigure = capturedCell ? capturedCell?.getFigure() : null;
 			} else {
 				capturedFigure = cellToMove.getFigure();
 			}
+
 			this.moveEmulator.emulateMove(figure, cellToMove);
 			const isCheck = this.isCheck(board, figure.getColor());
 			this.moveEmulator.revertMove(figure, cellToMove, figureCellBeforeMove, capturedFigure);
-			return !isCheck;
+
+			if (isCheck) return false;
+			if (figure.getFigureName() === Figures.King && this.moveAnalyzer.isCastlingMove(figure, cellToMove)) {
+				castlingMoves.push(cellToMove);
+				return false;
+			}
+			return true;
 		});
+		console.log(filteredFigureMoves);
+		castlingMoves = castlingMoves.filter(cellToMove => this.checkCastlingMovePossibility({ king: figure, cellToMove }, filteredFigureMoves));
+		console.log(castlingMoves);
+		return filteredFigureMoves.concat(castlingMoves);
+	}
+
+	/**
+	 * Checks if a castling move is possible.
+	 * @param king The king figure.
+	 * @param cellToMove The target cell for the castling move.
+	 * @param partlyFilteredCells The cells that are not under attack.
+	 * @returns True if the castling move is possible, false otherwise.
+	 */
+	private checkCastlingMovePossibility({ king, cellToMove }: { king: Figure; cellToMove: Cell }, partlyFilteredCells: Cell[]): boolean {
+		const kingCell = king.getCell();
+		const board = kingCell.getBoard();
+		const kingStartCol = kingCell.getColPos();
+		const targetCol = cellToMove.getColPos();
+
+		if (Math.abs(targetCol - kingStartCol) !== 2) {
+			return false;
+		}
+
+		const isShortCastling = targetCol < kingStartCol;
+		const isLongCastling = targetCol > kingStartCol;
+
+		if (isShortCastling) {
+			const requiredCells = [board.getCell(kingCell.getRowPos(), kingStartCol - 1)];
+			console.log(requiredCells);
+			console.log(partlyFilteredCells);
+			console.log(requiredCells.every(cell => partlyFilteredCells.some(filteredCell => filteredCell.getRowPos() == cell.getRowPos() && filteredCell.getColPos() == cell.getColPos())));
+			return requiredCells.every(cell => partlyFilteredCells.some(filteredCell => filteredCell.getRowPos() == cell.getRowPos() && filteredCell.getColPos() == cell.getColPos()));
+		}
+
+		if (isLongCastling) {
+			console.log('checcking long castling!');
+			const requiredCells = [board.getCell(kingCell.getRowPos(), kingStartCol + 1), board.getCell(kingCell.getRowPos(), kingStartCol + 2), board.getCell(kingCell.getRowPos(), kingStartCol + 3)];
+			return requiredCells.every(cell => partlyFilteredCells.some(filteredCell => filteredCell.getRowPos() == cell.getRowPos() && filteredCell.getColPos() == cell.getColPos()));
+		}
+		return false;
 	}
 }
 
